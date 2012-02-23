6d9e8ef598891777905fe12c38316afaf8831d6c
Sean Kennedy <sean.kennedy@elementive.com>
2012-02-22 21:51:56 -0600

    Fix dumbassery.

--- github/forks.py
+++ github/forks.py
@@ -7,14 +7,10 @@ class Forks:
     def listForks(self, repo, sort=None, user=None):
         username = self.__api.username if user == None else user
         if sort:
-            url = '%s?%s' % ('repos/%s/%s/forks' % (username, repo), sort)
+            url = '%s?sort=%s' % ('repos/%s/%s/forks' % (username, repo), sort)
         else:
             url = 'repos/%s/%s/forks' % (username, repo)
         return HTTP( self.__api ).get( url )
-
-    def listForks(self, repo, user=None):
-        username = self.__api.username if user == None else user
-        return HTTP( self.__api ).get( 'repos/%s/%s/forks' % (username, repo), )
 
     def createFork(self, repo, org=None, user=None):
         username = self.__api.username if user == None else user
