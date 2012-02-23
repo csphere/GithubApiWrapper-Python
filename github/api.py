76489110c3455d2fabec730acb9af6d1e29ce9f3
Sean Kennedy <sean.kennedy@elementive.com>
2012-02-23 12:53:01 -0600

    Oh.

--- github/api.py
+++ github/api.py
@@ -1,5 +1,6 @@
 from repos import Repos
 from users import Users
+from pullreq import PullReq
 
 class Api:
     apiurl = 'https://api.github.com'
@@ -7,5 +8,6 @@ class Api:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.pullrequests = PullReq( self )
         self.repos = Repos( self )
         self.users = Users( self )
